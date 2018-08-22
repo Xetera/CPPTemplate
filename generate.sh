8d4b7055f23548551cb8b7c672791be4761d6648
Unknown <mythologyali@gmail.com>
2018-08-21 18:07:39 -0700

    added #include and header guards to generate.sh

--- generate.sh
+++ generate.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+gen_src () {
+    touch "src/$1.cpp"
+    echo "#include \"$1.h\"" >> "src/$1.cpp"
+}
+
+gen_header () {
+    touch "include/$1.h"
+    echo "#pragma once" >> "include/$1.h"
+}
+
 if ! hash touch 2> /dev/null; then
     echo -e "\e[31m[ERROR] \e[0mAre you running on a linux VM? Your system doesn't seem to have a 'touch' command."
     exit 1
@@ -12,14 +22,14 @@ fi
 
 if [ -f "src/$1.cpp" ] && [ ! -f "include/$1.h" ]; then
     echo -e "\e[36m[INFO] \e[0mThe source file $1.cpp exists, creating header..."
-    touch "include/$1.h"
+    gen_header $1
     echo -e "\e[32m[SUCCESS] \e[0mFiles successfully created."
     exit 0
 fi
 
 if [ ! -f "src/$1.cpp" ] && [ -f "include/$1.h" ]; then
     echo -e "\e[36m[INFO] \e[0mThe header $1.h exists, creating cpp file..."
-    touch "src/$1.cpp"
+    gen_src $1
     echo -e "\e[32m[SUCCESS] \e[0mFiles successfully created."
     exit 0
 fi
@@ -30,8 +40,8 @@ if [ -f "src/$1.cpp" ] && [ -f "include/$1.h" ]; then
 fi
 
 {
-    touch src/$1.cpp
-    touch include/$1.h
+    gen_src $1
+    gen_header $1
     echo -e "\e[32m[SUCCESS] \e[0mFiles successfully created."
     } || {
     echo -e "\e[31m[ERROR] \e[0mThere was a problem creating these files."
